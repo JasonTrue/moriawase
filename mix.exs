fb3ac28eda2d8770646b935ab619aff89fb675bb
Jason Truesdell <jason@yuzuten.com>
2022-12-28 17:20:15 +0900

    Add ex_machina and faker

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule Moriawase.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -48,8 +47,9 @@ defmodule Moriawase.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
-    ]
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_machina, "~> 2.7", only: [:dev, :test]},
+      {:faker, "~> 0.17.0", only: [:dev, :test]}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
